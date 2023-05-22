382157d7e859f2e25c994f62a087e0eca86e6b30
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2023-05-22 15:18:35 -0300

    Criando classe DB com as funções responsaveis para criar a tabela generica e popular com os dados passado pelo arquivo JSON.

--- db.py
+++ db.py
@@ -11,18 +11,74 @@ user = os.getenv('USER')
 password = os.getenv('PASSWORD')
 port = os.getenv('PORT')
 
-connection = psycopg2.connect(
-    dbname= database,
-    host= host,
-    user= user, 
-    password= password,
-    port= port
-)
+class DB:
+    
+    connection = psycopg2.connect(
+        dbname= database,
+        host= host,
+        user= user, 
+        password= password,
+        port= port
+    )
+    
 
-print(connection)
-with connection:
-    with connection.cursor() as curs:
+    print(connection)
+    
+    def create(self, name : str, columns : list):
+        drop_table_if_exists_query = f"DROP TABLE IF EXISTS public.{name}"
+        
+        # Caso a tabela já exista no banco ela deve ser apagada previamanete
         try:
-            curs.execute("INSERT INTO public.mesas(id, lugares, livre) VALUES (50, 50, 50)")
+            with self.connection.cursor() as cursor:
+                try:
+                    cursor.execute(drop_table_if_exists_query)
+                except:
+                    print("Ocorreu um erro ao dropar a tabela.")
         except:
-            print("deu ruim")
+            print("Ocorreu um erro ao tentar obter o cursor.")
+
+        create_query = f"CREATE TABLE IF NOT EXISTS public.{name} ("
+        
+        # Percorre as colunas na lista de colunas e adiciona ela na create_query
+        for column in columns:
+            create_query += column + " varchar(200)" + ( "," if (column != columns[-1]) else "" )
+        
+        create_query += " )"
+        
+        # Efetua a criação da tabela
+        with self.connection.cursor() as cursor:
+            try:
+                cursor.execute(create_query)
+            except:
+                print("Ocorreu um erro ao criar a tabela.")
+            finally:
+                cursor.close()
+                self.connection.commit()
+        
+        
+    
+    
+    def fill_table(self, name : str, dados : list):
+        insert_query = f"INSERT INTO public.{name} VALUES"
+        
+        # Concatenando os dados
+        for i in range(len(dados[0])):
+            insert_query += " ("
+            for j in range(len(dados)):
+                insert_query += f"{dados[j][i]}" + ( "," if (j != len(dados)-1) else "" )
+                
+            insert_query += ( ")," if (i != len(dados[0])-1) else ") " )
+        
+        # Executando a query para inserção
+
+        with self.connection.cursor() as cursor:
+            try:
+                cursor.execute(insert_query)
+            except:
+                print("Ocorreu um erro ao criar a tabela.")
+            finally:
+                cursor.close()
+                self.connection.commit()
+       
+        
+        
--- read_metadata.py
+++ read_metadata.py
@@ -1,6 +1,30 @@
 import json
+from db import DB
 
 with open("metadado.json") as my_json:
     data = json.load(my_json)
 
 print(data)
+print(data.keys())
+tables = data.keys()
+for table in tables:
+    print(table)
+print(table)
+
+print(data[table])
+
+columns_list = list()
+dados_list = list()
+
+for column in data[table]:
+    columns_list.append(column)
+    dados_list.append(data[table][column])
+
+print(columns_list)
+print(dados_list)
+    
+
+database = DB()
+
+database.create(table, columns_list)
+database.fill_table(table, dados_list)
